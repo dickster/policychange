20055e60c0455be29004b4be85483502f79a67e6
Derek Dick (RFS/RCS/PC <derek.dick@towerswatson.com>
2017-04-24 14:27:10 -0400

    refactor Utils to have common starting script to load config & changes.

--- peel/Utils.js
+++ peel/Utils.js
@@ -1,6 +1,43 @@
 var Utils = (function() {
 
+    var param = function(name, defaultValue) {
+        var p = (window.location.search.split(name + '=')[1] || '').split('&')[0];
+        return p ? p : defaultValue;
+    };
+
+    var getJson = function(parameter, dflt, callback) {
+        // if specified in URL, that wins otherwise we'll use default.
+        var file = param(parameter,dflt);
+        file = 'data/' + file;
+        if (file.indexOf('.json')==-1) {
+            file = file + '.json';
+        }
+        console.log('loading data from ' + file);
+        return $.getJSON(file, callback);
+    };
+
     return {
+
+        debugChangeEditor: function(changes,config) {
+            var options = {};
+
+            if (!changes) changes = 'changes';   //use default file name if none provided.
+            if (!config) config = 'config';
+
+            var assignChanges = function(json) { options.changes=json;};
+            var assignConfig = function(json) { options.config=json;};
+
+            $.when( getJson('changes', changes, assignChanges),
+                    getJson('config', config, assignConfig) )
+                .then( function() {
+                    // this is the only line you need to start editor.
+                    // the rest is there to facilitate loading test data based on url parameters (QA use case only).
+                    wtw.changeEditor.init(options);
+                }, function() {
+                    alert('cant load config/changes test data');
+                } );
+
+        },
         ensureVisible : function($el,$container,scroll) {
             if (!$container) {   // assume it's the immediate parent.
                 $container = $el.parent();
